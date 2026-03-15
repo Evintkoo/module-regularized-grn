606f8bdfd780ff9fc1a4d1b3bdc9b3d0afae2586
Evintkoo <evint.koo@gmail.com>
2026-03-15 11:14:49 +0700

    chore: delete dead model files, clean models/mod.rs

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,28 +1,10 @@
 pub mod nn;
-pub mod embeddings;
-pub mod two_tower;
-pub mod baseline;
-pub mod learnable_embeddings;
-pub mod expression_model;
-pub mod optimized_embeddings;
 pub mod hybrid_embeddings;
-pub mod scalable_hybrid;
-pub mod attention;
-pub mod attention_model;
-pub mod classifier_head;
-pub mod cross_attention_model;
 
-pub use nn::{LinearLayer, Dropout, relu, relu_backward, sigmoid, sigmoid_backward, bce_loss, bce_loss_backward};
-pub use cross_attention_model::{CrossAttentionModel, ModelAdamStates};
-pub use learnable_embeddings::{LearnableEmbedding, EmbeddingTwoTower};
-pub use expression_model::ExpressionTwoTower;
-pub use optimized_embeddings::{OptimizedEmbeddingModel, focal_loss, focal_loss_backward};
+pub use nn::{
+    LinearLayer, Dropout,
+    relu, relu_backward,
+    sigmoid, sigmoid_backward,
+    bce_loss, bce_loss_backward,
+};
 pub use hybrid_embeddings::HybridEmbeddingModel;
-pub use scalable_hybrid::{ScalableHybridModel, ModelConfig};
-pub use attention::SelfAttention;
-pub use attention_model::AttentionExpressionModel;
-pub use classifier_head::{ClassificationHead, HybridWithClassifier};
-
-pub use embeddings::*;
-pub use two_tower::*;
-pub use baseline::*;
